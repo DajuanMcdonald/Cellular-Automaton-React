eb826faedc7290699cf0c1627597905b6ffd73c8
fiopwk <dajuan-mcdonald@lambdastudents.com>
2020-09-17 05:29:22 -0400

    about component

---
+++ app/appabout.js
@@ -0,0 +1,12 @@
+const { createContext } = require("react");
+
+import React from 'react';
+
+const AppAbout = () => {
+    return (
+        <React.Fragment>
+
+        </React.Fragment>
+    )
+}
+export default AppAbout;
