8dd6cf1badd66085db6b8363ac6bb6f41352a3cd
fiopwk <dajuan-mcdonald@lambdastudents.com>
2020-09-17 05:07:07 -0400

    pluging button to app

--- app/index.js
+++ app/index.js
@@ -1,15 +1,22 @@
 import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { useEffect, useState } from 'react';
 
 const App = () => {
 
         return (
             <div className='App'>
                 <h1>Cellular Atomaton: Conway's Game of Life</h1>
+
+                <React.Fragment>
+                    <button>Start</button>
+                    <button>Stop</button>
+                    <button>Step</button>
+                    <button>Random</button>
+                </React.Fragment>
                
             </div>
+            
         )
 }
 
