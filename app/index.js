fc2dae5e6a1f5067bf7b0c3fa6fcbc61af250f0d
fiopwk <dajuan-mcdonald@lambdastudents.com>
2020-09-17 04:32:31 -0400

    track cells state inside the Hook.

--- app/index.js
+++ app/index.js
@@ -1,15 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
+import React, {useState, useEffect} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { useEffect, useState } from 'react';
 
-class App extends React.Component {
-    render() {
+const App = () => {
+
         return (
             <div className='App'>
                 <h1>Cellular Atomaton: Conway's Game of Life</h1>
+               
             </div>
         )
-    }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'))
