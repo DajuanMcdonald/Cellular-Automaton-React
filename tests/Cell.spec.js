e79c669e4248ed679f3d59c985a532a9f025b919
fiopwk <dajuan-mcdonald@lambdastudents.com>
2020-09-21 21:54:31 -0400

    fixed rules logic and finished testing basic rules

--- tests/Cell.spec.js
+++ tests/Cell.spec.js
@@ -1,39 +1,45 @@
 import Cell from '../src/components/Cell';
 import CellState from '../src/components/CellState';
 
-describe('Testing Cell State', () => {
+    describe('Testing Cell State', () => {
 
-    const aliveCell = new Cell(CellState.ALIVE)
-    const deadCell = new Cell(CellState.DEAD)
+        const aliveCell = new Cell(CellState.ALIVE)
+        const deadCell = new Cell(CellState.DEAD)
 
-    test('Should have initialized with a cell state', () => {
-        expect(aliveCell.state).toEqual(CellState.ALIVE)
-        expect(deadCell.state).toEqual(CellState.DEAD)
+        //initial state of cells
+        test('Should have initialized with a cell state', () => {
+            expect(aliveCell.state).toEqual(CellState.ALIVE)
+            expect(deadCell.state).toEqual(CellState.DEAD)
+            
+        })
+
+        //dead cell should stay dead and has zero live neighbors
+        test('Cell with no live neighbors', () => {
+            const nextStateWith0Neighbors = aliveCell.getNeighbors(0)
+            expect(nextStateWith0Neighbors).toEqual(CellState.DEAD)
+        })
+
+        //should die if fewer than 2 neighbors
+        test('Cell with 1 live neighbors', () => {
+            const nextState = aliveCell.getNeighbors(1)
+            expect(nextState).toEqual(CellState.DEAD)
+        })
+
+        // should live with 2 or 3 live neighbors
+        test('Cell with 2 or 3 live neighbors', () => {
+
+            const nextStatewith2Neighbors = aliveCell.getNeighbors(2);
+            expect(nextStatewith2Neighbors).toEqual(CellState.ALIVE);
+
         
+            const nextStatewith3Neighbors = aliveCell.getNeighbors(3);
+            expect(nextStatewith3Neighbors).toEqual(CellState.ALIVE);
+        })
+
+        //should die if more than 3 neighbors
+        test('Cell with more than 3 live neighbors', () => {
+            const nextStatewithMoreThan3Neighbors = aliveCell.getNeighbors(4);
+            expect(nextStatewithMoreThan3Neighbors).toEqual(CellState.DEAD)
+        })
+
     })
-
-    test('Should die if zero neighbors', () => {
-        const nextStateWith0Neighbors = aliveCell.getNextState(0)
-        expect(nextStateWith0Neighbors).toEqual(CellState.DEAD)
-    })
-
-    test('Should die if it has fewer than 2 live neighbors', () => {
-        const nextState = aliveCell.getNextState(1)
-        expect(nextState).toEqual(CellState.DEAD)
-    })
-
-    test('Should live with 2 or 3 live neighbors', () => {
-
-        const nextStatewith2Neighbors = aliveCell.getNextState(2);
-        expect(nextStatewith2Neighbors).toEqual(CellState.ALIVE);
-
-       
-        const nextStatewith3Neighbors = aliveCell.getNextState(3);
-        expect(nextStatewith3Neighbors).toEqual(CellState.ALIVE);
-    })
-
-    test('Should die with more than 3 neighbors', () => {
-        const nextStatewithMoreThan3Neighbors = aliveCell.getNextState(4);
-        expect(nextStatewithMoreThan3Neighbors).toEqual(CellState.DEAD)
-    })
-})
