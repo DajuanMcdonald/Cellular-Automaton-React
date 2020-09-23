4d05aadb9f4483875d718e2b086e7930bc477824
fiopwk <dajuan-mcdonald@lambdastudents.com>
2020-09-23 19:54:29 -0400

    added some styling to visualize plan

--- src/App.js
+++ src/App.js
@@ -13,6 +13,10 @@ class App extends React.Component {
 
     return (
       <>
+     
+  
+
+<div id="output"></div>
       <div className='main'>
         <h1>
           Conway's Game of Life
@@ -22,6 +26,7 @@ class App extends React.Component {
         <div className='options'>
           {/** Create a few sample cell configurations that users can load and runAdd an option that creates a random cell configuration that users can run */}
           <Option/>
+  
         </div>
         
         <div className='right-bar'>
