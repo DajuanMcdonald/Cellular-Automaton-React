4536bf2725ba0117c3efa1c031ce0cf10da6f197
fiopwk <dajuan-mcdonald@lambdastudents.com>
2020-09-23 20:32:25 -0400

    added Bootstrap component for modal and options drop down menu

--- src/components/Options.js
+++ src/components/Options.js
@@ -5,6 +5,31 @@ function Option() {
     return (
         <>
         <p>Options:</p>
+        <div className="d-flex">
+            <div className="dropdown mr-1">
+                <button type="button" className="btn btn-secondary dropdown-toggle" id="dropdownMenuOffset" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-offset="10,20">
+                Speed
+            </button>
+            <div className="dropdown-menu" aria-labelledby="dropdownMenuOffset">
+                <a className="dropdown-item" href="#">1</a>
+                <a className="dropdown-item" href="#">2</a>
+                <a className="dropdown-item" href="#">3</a>
+                </div>
+            </div>
+            <div className="btn-group">
+                <button type="button" className="btn btn-secondary">Configure</button>
+                <button type="button" className="btn btn-secondary dropdown-toggle dropdown-toggle-split" id="dropdownMenuReference" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-reference="parent">
+                <span className="sr-only">Toggle Dropdown</span>
+                </button>
+                <div className="dropdown-menu" aria-labelledby="dropdownMenuReference">
+                <a className="dropdown-item" href="#">Glider</a>
+                <a className="dropdown-item" href="#">Toad</a>
+                <a className="dropdown-item" href="#">Oscillator</a>
+                <div className="dropdown-divider"></div>
+                <a className="dropdown-item" href="#">Spaceship</a>
+                </div>
+            </div>
+        </div>
         </>
     )
 
